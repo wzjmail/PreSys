ff3066ac9221e3ff7d1efa673561839d3d6ebe04
wzjmail <zjwangmail@163.com>
2017-12-01 10:32:20 +0800

    1

---
+++ CMS/src/com/wzj/ms/message/User.java
@@ -0,0 +1,83 @@
+package com.wzj.ms.message;
+
+public class User extends Common
+{
+  private String image;
+private int newNum;
+private int newNumB;
+private int num;
+private int numAll;
+private int number;
+private String password;
+private int role;
+private String session;
+private double price;
+public double getPrice() {
+	return price;
+}
+
+
+public void setPrice(double price) {
+	this.price = price;
+}
+
+  public String getImage() {
+    return this.image;
+  }
+public int getNewNum()
+  {
+    return this.newNum;
+  }
+public int getNewNumB() {
+    return this.newNumB;
+  }
+public int getNum() {
+    return this.num;
+  }
+public int getNumAll() {
+    return this.numAll;
+  }
+public int getNumber() {
+    return this.number;
+  }
+public String getPassword() {
+    return this.password;
+  }
+public int getRole() {
+    return this.role;
+  }
+public String getSession() {
+    return this.session;
+  }
+public void setImage(String image) {
+    this.image = image;
+  }
+public void setNewNum(int newNum) {
+    this.newNum = newNum;
+  }
+public void setNewNumB(int newNumB) {
+    this.newNumB = newNumB;
+  }
+public void setNum(int num) {
+    this.num = num;
+  }
+public void setNumAll(int numAll) {
+    this.numAll = numAll;
+  }
+public void setNumber(int number) {
+    this.number = number;
+  }
+public void setPassword(String password) {
+    this.password = password;
+  }
+public void setRole(int role) {
+    this.role = role;
+  }
+public void setSession(String session) {
+    this.session = session;
+  }
+public String toString() {
+    return "User [session=" + this.session + ", password=" + this.password + ", role=" + this.role + 
+      ", getSign()=" + getSign() + "]";
+  }
+}
